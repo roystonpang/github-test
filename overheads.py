43640d9dc445cc47a1408138d9ebde655c53c117
bhnhoon <110555638+bhnhoon@users.noreply.github.com>
2022-08-04 13:11:22 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -1,1 +1,23 @@
-hello
+from pathlib import Path
+import re, csv
+
+def overhead_function(forex):
+    """
+    - function will determine the highest overhead category and its value (converted from USD to SGD)
+    - parameter forex is predetermined, no other parameter required
+    """
+    # file path for overheads.csv is created
+    # the data in overheads.csv is actual usd figures and not in percentages
+    home = Path.cwd()/'csv_reports'/'overheads.csv'
+    
+    # empty list to store data extracted from overheads.csv
+    expenses_list = []
+
+    # context manager to open file in "read" mode
+    with home.open(mode='r',encoding='UTF-8',newline='') as file:
+        reader = csv.reader(file)
+        # for loop is used and all data is appeneded to the empty list as dictionary
+        for line in reader:
+            expenses_list.append({"category": line[0], "overheads": line[1]})
+    # header is removed
+    expenses_list.pop(0)
