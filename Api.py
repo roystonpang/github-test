7386d4000fedba3a01bb32037c4ef28ed6963e72
lee-Jia-Wen <110384566+lee-Jia-Wen@users.noreply.github.com>
2022-08-04 13:10:49 +0800

    Create Api.py

---
+++ Api.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+def api_function():
+    """
+    - function will determine real time 
+    currency exchange rate from USD to SGD
+    - no parameter required
+    """
+    # please insert your personal api key to access alphavantage data
+    api_key = ""
+    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+
+    # use get method from requests on the api url
+    response = requests.get(url)
+
+    # use json method from requests to get data
+    forex = response.json()
+   
+    # extract conversion value from the key: "Realtime Currency Exchange Rate""5. Exchange Rate"
+    return float(forex["Realtime Currency Exchange Rate"]["5. Exchange Rate"])   
+
+api_function()
